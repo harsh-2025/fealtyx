2abdb0360ac6624cf7f491b47d4e6afbca4c8611
Harsh Rawat <harshofficialrawat@gmail.com>
2024-06-12 21:28:14 +0530

    1st step done

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,2 @@
-// src/pages/Dashboard.js
-import React from 'react';
-import { useAuth } from '../AuthContext';
 
-const Dashboard = () => {
-  const { logout } = useAuth();
 
-  return (
-    <div>
-      <h2>Dashboard</h2>
-      {/* <button onClick={logout}>Logout</button> */}
-    </div>
-  );
-};
-
-export default Dashboard;
